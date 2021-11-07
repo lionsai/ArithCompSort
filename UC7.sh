d2d8344f9b60006ac13396b4d5eea29693e32c07
lionsai <goripartisaikrishna@gmail.com>
2021-11-07 23:09:48 +0530

    ADDED UC7.sh

---
+++ UC7.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "a: " a
+read -p "b: " b
+read -p "c: " c
+
+p=$(($a+$b*$c))
+q=$(($a*$b+$c))
+r=$(($a/$b+$c))
+s=$(($a%$b+$c))
+
+comp[1]=$p
+comp[2]=$q
+comp[3]=$r
+comp[4]=$s
+
+echo "dictionary: " ${!comp[@]} "--> " ${comp[@]}
+declare -a arr
+
+for((i=1; i<=4; i++))
+do
+        arr[$i]=${comp[i]}
+        echo $i ">" ${comp[i]}
+
+done
+
+echo ${arr[@]}
